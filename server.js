30cc0211630cbc4cc1180ab4382a1008b926205f
Yeo Shu Yi <yeoshuyi123@gmail.com>
2026-06-09 09:45:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on port ${PORT}`);
 });
